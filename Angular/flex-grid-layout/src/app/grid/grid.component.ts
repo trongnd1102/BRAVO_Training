4c20a187e7eabfe7a74ea101e55b3556c764904d
trongnd1102 <trongnd.1102@gmail.com>
2022-05-20 14:32:56 +0700

    sử dụng group

--- Angular/flex-grid-layout/src/app/grid/grid.component.ts
+++ Angular/flex-grid-layout/src/app/grid/grid.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { CollectionView }  from '@grapecity/wijmo';
-import * as wjGrid from '@grapecity/wijmo.grid';
 import { AllowSorting }from '@grapecity/wijmo.grid';
-import { WjGridFilterModule } from '@grapecity/wijmo.angular2.grid.filter';
-import { WjGridSearchModule } from '@grapecity/wijmo.angular2.grid.search';
+// import * as productData from '../../assets/product.json'
+import * as wjGrid from '@grapecity/wijmo.grid';
 
 @Component({
   selector: 'app-grid',
@@ -15,19 +14,29 @@ export class GridComponent implements OnInit {
   @ViewChild('grid', { static: true }) grid!: wjGrid.FlexGrid;
 
   products: any;
+  datas: any;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+    this._getData()
+  }
 
   ngOnInit(): void {
-    this._getData();
   }
 
   private _getData() {
     // get data from JSON file
     this.httpClient.get("assets/product.json").subscribe((data) => {
       // create a paged CollectionView with 12 items per page
-      this.products = new CollectionView(data, {pageSize: 12});
+      this.products = new CollectionView(data, {
+        pageSize: 100,
+        sortDescriptions: [ 'Unit', 'ItemGroupCode' ],
+        groupDescriptions: [ 'Unit', 'ItemGroupCode' ]
+      });
     })
+  }
+
+  initializeGrid(grid: wjGrid.FlexGrid) {
+    grid.select(new wjGrid.CellRange(0,0), true);
   }
 
   // Styling rows
@@ -61,7 +70,7 @@ export class GridComponent implements OnInit {
     }
   }
 
-  allowSorting = AllowSorting.None;
+  allowSorting = AllowSorting.MultiColumn;
   allowSortingOptions = 'None,SingleColumn,MultiColumn'.split(',');
 
   // change the allowSorting value
@@ -69,5 +78,13 @@ export class GridComponent implements OnInit {
       this.products.sortDescriptions.clear(); // remove current sort
       this.allowSorting = value; // apply the new setting
   }
+
+  // filter by country
+  filter(e: any) {
+    let filter = (<HTMLInputElement>e.target).value.toLowerCase();
+    this.grid.collectionView.filter = (item: any) => {
+        return filter.length == 0 || item.Unit.toLowerCase().indexOf(filter) > -1
+    }
+}
 
 }
