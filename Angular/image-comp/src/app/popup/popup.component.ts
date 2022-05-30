77ac1d5f84b234b804d7477a27a0217325140173
trongnd1102 <trongnd.1102@gmail.com>
2022-05-30 13:02:27 +0700

    responsive html & covert url

--- Angular/image-comp/src/app/popup/popup.component.ts
+++ Angular/image-comp/src/app/popup/popup.component.ts
@@ -9,6 +9,7 @@ import * as wijiInput from '@grapecity/wijmo.input'
 export class PopupComponent implements OnInit  {
   @ViewChild('ImagePopup', {static: true}) imagePopup!: wijiInput.Popup;
   url!: string;
+  urlCopy: any;
 
   constructor() { }
 
@@ -18,5 +19,24 @@ export class PopupComponent implements OnInit  {
 
   getImageUrl(e: any) {
     this.url = e;
+    this.urlCopy = e;
+  }
+
+  convertBase64ToUint8Array(pUrl: any) {
+    var BASE64_MARKER = ';base64,';
+    var base64Index = pUrl.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    var base64 = pUrl.substring(base64Index);
+    var raw = window.atob(base64);
+    var rawLength = raw.length;
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+    for(let i = 0; i < rawLength; i++) {
+      array[i] = raw.charCodeAt(i);
+    }
+    this.urlCopy = "data:image/jpeg;uint8array," + array
+  }
+
+  converUint8ArrayToBase64() {
+    this.urlCopy = this.url;
   }
 }
