c20d8ff349fe11b50452c1c0166eedeacc02ac02
trongnd1102 <trongnd.1102@gmail.com>
2022-06-03 16:49:38 +0700

    complete apply image form

--- Angular/image-form/src/app/apply-form/apply-form.component.ts
+++ Angular/image-form/src/app/apply-form/apply-form.component.ts
@@ -11,9 +11,7 @@ export class ApplyFormComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
-      Image1: ['', {updateOn: 'change'}],
-      Image2: ['', {updateOn: 'change'}],
-      Image3: ['', {updateOn: 'change'}],
+      Image: ['', {updateOn: 'change'}],
     });
   }
 
@@ -21,21 +19,5 @@ export class ApplyFormComponent implements OnInit {
     this.form.valueChanges.subscribe(value => {
       console.log(value)
     })
-  }
-
-  get Image1() {
-    return this.form.get('Image1')!;
-  }
-
-  get Image2() {
-    return this.form.get('Image2')!;
-  }
-
-  get Image3() {
-    return this.form.get('Image3')!;
-  }
-
-  convertBase64ToImage(e: any, pFormName: any) {
-    this.form.get(pFormName)!.setValue(e.target.value);
   }
 }
